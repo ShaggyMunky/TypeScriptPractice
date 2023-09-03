9ef35ac3be55c5cb1ac54ec3022e62f332ece619
Aaron Guillen <aaronguillen21@gmail.com>
2023-09-03 02:29:09 -0700

    fucntion types and callbacks

--- app.ts
+++ app.ts
@@ -10,6 +10,11 @@ function printResult(num: number): void {
     console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void){
+    const result = n1 + n2;
+    cb(result);
+}
+
 printResult(add(5, 12));
 
 //defining function variables
@@ -20,3 +25,8 @@ combineValues = add;
 
 
 console.log(combineValues(8, 8))
+
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+});

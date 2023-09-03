4325533cd284f074ac4df737fceba955a01dfe0a
Aaron Guillen <aaronguillen21@gmail.com>
2023-09-03 01:26:36 -0700

    tuple types

--- app.ts
+++ app.ts
@@ -6,10 +6,27 @@ let personTemplate: {
     age: number;
 };
 
+//Other scenarion to use key:type assignment is with a clearly defined Tuple
+//by defining a Tuple, it indicates ecactly what is allowed and where
+const personTuple: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+    roleBad: [number, string]
+} = {
+    name: "Harold",
+    age: 26,
+    hobbies: ['Sports', 'Cooking'],
+    roleBad: ['author', 2] //Type error thrown
+};
+
+
 const person = {
     name: "Harold",
     age: 26,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'] //Tuple type
 };
 
 //setting the kind of array with the type of which it consists

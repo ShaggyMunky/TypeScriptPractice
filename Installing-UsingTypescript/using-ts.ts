2447ebf1c40db5bbde8fea36d9ffc2323eb4971b
Aaron Guillen <aaronguillen21@gmail.com>
2023-09-02 22:58:21 -0700

    module installing and using typescript

---
+++ Installing-UsingTypescript/using-ts.ts
@@ -0,0 +1,12 @@
+//some linting errors are from the js-only file since the same variables exist
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement; //! means will never yield null and second part is typecasting
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function() {
+  console.log(add(+input1.value, +input2.value));
+});

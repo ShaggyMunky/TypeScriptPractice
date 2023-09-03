ecd29b060fd463c3cbb42a028ab1bf5dfcfab943
Aaron Guillen <aaronguillen21@gmail.com>
2023-09-03 00:57:47 -0700

    defining types on variables

--- app.ts
+++ app.ts
@@ -8,10 +8,15 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string){ //typ
     
 }
 
-const number1 = 5;
+//assigning type at variable instantiation not recommended due to redundancy
+//and type inference. Only recomennded if variable not initialized when declared
+let noNumber: number = 5; //not recommended
+let okNumber: number; //ok
+let number1 = 5; //ok
+
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: '
+const resultPhrase = 'Result is: ';
 
 add(number1, number2, printResult, resultPhrase);
 

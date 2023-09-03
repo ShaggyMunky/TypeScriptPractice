2c3f7a1c347459abe72ece05cbac644402e7bdd3
Aaron Guillen <aaronguillen21@gmail.com>
2023-09-03 01:58:23 -0700

    literal types

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-function combine(input1, input2) {
+//Union types allow for multiple types of parameters to be used
+//Add uniong types with the '|'. Can add more than two different types on one parameter
+//It may be required to have checkes performed on the types being used as the linting can throw an error if it's not 100% sure
+//the actions of the two types will work
+//literal types indicate what the exact value that can be held
+//
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
-        input1.toString() + input2.toString();
+        result = input1.toString() + input2.toString();
     }
     return result;
+    // if(resultConversion === 'as-number'){
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Max", "Ana");
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Ana", "as-text");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,19 +1,29 @@
 //Union types allow for multiple types of parameters to be used
+//Add uniong types with the '|'. Can add more than two different types on one parameter
 //It may be required to have checkes performed on the types being used as the linting can throw an error if it's not 100% sure
 //the actions of the two types will work
 
-function combine(input1: number | string, input2: number | string) {
+//literal types indicate what the exact value that can be held
+
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text' /*literal type*/) {
     let result
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number' ){
+        result = +input1 + +input2;
     } else {
-        input1.toString() + input2.toString();
+        result = input1.toString() + input2.toString();
     }
+
     return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Ana")
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Ana", "as-text")
 console.log(combinedNames);
